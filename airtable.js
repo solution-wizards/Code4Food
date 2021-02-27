933f81dddc53a0329c521ad66457a1fef81eada9
wade-pjmartin <70186676+wade-pjmartin@users.noreply.github.com>
2021-02-27 09:26:12 -0600

    preparing skeleton to be editable using Airtable

---
+++ airtable.js
@@ -0,0 +1,29 @@
+var Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: 'YOUR_API_KEY'
+});
+var base = Airtable.base('appePiECcTIBWPH0R');
+
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'key3c3ekykKo7uJhd'}).base('appePiECcTIBWPH0R');
+
+base('code4food').select({
+    // Selecting the first 3 records in Site Content:
+    maxRecords: 3,
+    view: "Site Content"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('id'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
